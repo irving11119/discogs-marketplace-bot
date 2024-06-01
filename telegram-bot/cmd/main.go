b819772340431db497a8df80b2060ddf1f8a12d8
irving11119 <irving.deboer@gmail.com>
2024-06-02 00:14:45 +0800

    feature: create directory structure and telegram bot skeleton
    Create project directory for telegram bot and interface for Discogs API.
    
    Wrote initial skeleton code for Telegram Bot in Go.

---
+++ telegram-bot/cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+// Send any text message to the bot after the bot has been started
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(os.Getenv("TELEGRAM_API_BOT"), opts...)
+	if nil != err {
+		// panics for the sake of simplicity.
+		// you should handle this error properly in your code.
+		panic(err)
+	}
+
+	WebHookUrl := os.Getenv("WEBHOOK_URL")
+	res, err := b.SetWebhook(ctx, &bot.SetWebhookParams{
+		URL: WebHookUrl,
+	})
+
+	if !res || err != nil {
+		log.Fatalf("Error failed to set WebHook.")
+	}
+
+	go func() {
+		err := http.ListenAndServe(":8080", b.WebhookHandler())
+
+		if err != nil {
+			log.Fatalf("Failed to start server")
+		}
+	}()
+
+	// Use StartWebhook instead of Start
+	b.StartWebhook(ctx)
+
+	// call methods.DeleteWebhook if needed
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
+
+	if err != nil {
+		log.Println("Failed to send reply to bot")
+	}
+}
---
+++ telegram-bot/go.mod
@@ -0,0 +1,6 @@
+module github.com/irving11119/discord-marketplace-bot
+
+go 1.21.3
+
+require github.com/go-telegram/bot v1.4.0
+
